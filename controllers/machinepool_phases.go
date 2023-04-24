089c3497c4727288ff4463635ae9b8842242015a
Dustin Scott <dscott@redhat.com>
2023-04-24 09:40:51 -0500

    chore: add reconciling again message
    Signed-off-by: Dustin Scott <dscott@redhat.com>

--- controllers/machinepool_phases.go
+++ controllers/machinepool_phases.go
@@ -261,6 +261,7 @@ func (r *MachinePoolReconciler) Complete(request *MachinePoolRequest) (ctrl.Resu
 	}
 
 	request.Log.Info("completed machine pool reconciliation", request.logValues()...)
+	request.Log.Info(fmt.Sprintf("reconciling again in %d minutes", r.Interval), request.logValues()...)
 
 	return requeueAfter(r.Interval), nil
 }
